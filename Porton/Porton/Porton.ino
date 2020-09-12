a369569a911f537d022afe5aed9b8a8c6f9e64b9
Jacqueline06 <jacquelinemendez061@gmail.com>
2020-09-11 21:59:13 -0600

    Porton :smiley:
    Falta aun el buzzer , la pantalla

--- Porton/Porton/Porton.ino
+++ Porton/Porton/Porton.ino
@@ -7,8 +7,12 @@ int posicion = 0;
 int portonAbierto = 4;//led porton abierto ROJO
 int portonCerrado = 7;//led porton cerrado AMARRILLO
 int WIFI = 12; //Señal de la app
+//int buzzerP = 8; //buzzer de cerrado
+
 void setup() {
-//Servo
+  Serial.begin(9600);
+
+  //Servo
   pinMode(PORTON, OUTPUT);
   pinMode(portonAbierto, OUTPUT);
   pinMode(portonCerrado, OUTPUT);
@@ -16,43 +20,48 @@ void setup() {
 }
 
 void loop() {
-
- controlPorton();
-
+  controlPorton();
 }
 
 void controlPorton() {
-if (WIFI == HIGH){ //encender led por 6 seg al terminar se cierra el porton
-  abrirPorton();
-  digitalWrite(portonAbierto, HIGH); //No enciende la led Roja
-  delay(6000);
-  digitalWrite(portonAbierto, LOW);
-  WIFI = LOW;
-} else if (WIFI == LOW){        //Controlar que cuando se apague si se apague y no esperar a los 6 seg
-  cerrarPorton();
-  digitalWrite(portonCerrado, HIGH);
-  delay(3000);
-  digitalWrite(portonCerrado, LOW);
-} else { // apagar led roja, cerrar porton. led amarilla y buzer encendido 3 seg
-   cerrarPorton();
-  digitalWrite(portonCerrado, HIGH);
-  delay(3000);
-  digitalWrite(portonCerrado, LOW);
+  posicion = digitalRead(WIFI);
+
+  if (posicion == 1) { //encender led por 6 seg al terminar se cierra el porton
+    Serial.println(">> HIGH");
+
+    abrirPorton();
+
+    Serial.println(">> LED ROJA");
+    digitalWrite(portonAbierto, HIGH); //No enciende la led Roja
+    delay(6000);
+    digitalWrite(portonAbierto, LOW);
+
+    posicion = LOW;
+    cerrarPorton();
+
+  } else {       //Controlar que cuando se apague si se apague y no esperar a los 6 seg
+    Serial.println(">> LOW");
+    cerrarPorton();
   }
 }
 
-void abrirPorton(){             // 2 vueltas derecha
-   motor.attach(PORTON);
-  for (int i = 0; i <= 720; i++) {
+void abrirPorton() {            // 2 vueltas derecha
+  motor.attach(PORTON);
+  for (int i = 0; i <= 180; i++) {
     motor.write(i);
     delay(25);
   }
 }
 
-void cerrarPorton(){      // 2 vueltas izquierda
- motor.attach(PORTON); 
-  for (int i = 719; i>0; i--){
+void cerrarPorton() {     // 2 vueltas izquierda
+  motor.attach(PORTON);
+  for (int i = 180; i >= 0; i--) {
     motor.write(i);
     delay(25);
   }
+
+  Serial.println(">> LED AMARILLA");
+  digitalWrite(portonCerrado, HIGH);
+  delay(3000);
+  digitalWrite(portonCerrado, LOW);
 }
